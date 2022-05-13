f7d5cc99def3276057fc96b0e28e5e9faee685db
haixuanTao <tao.xavier@outlook.com>
2022-05-13 14:10:57 +0200

    Renaming operator to node

--- launch_in_container.sh
+++ launch_in_container.sh
@@ -2,11 +2,12 @@
 cd /home/erdos/workspace/dora-rs
 export PYTHONPATH=$PYTHONPATH:$(pwd)/nodes
 # export RUST_LOG=warn
-./dora-rs start-python carla_source_operator send &
-./dora-rs start-python perfect_detection_operator run pose depth_frame segmented_frame &
-./dora-rs start-python obstacle_location_operator run pose depth_frame obstacles_without_location &
-./dora-rs start-python planning_operator run pose obstacles &
-./dora-rs start-python pid_control_operator run pose waypoints &
-./dora-rs start-python control_operator run control vehicle_id &
-# ./dora-rs start-python summary_operator run control_status pose obstacles &
+./dora-rs start-python carla_source_node send &
+./dora-rs start-python perfect_detection_node run pose depth_frame segmented_frame &
+./dora-rs start-python obstacle_location_node run pose depth_frame obstacles_without_location &
+./dora-rs start-python planning_node run pose obstacles &
+./dora-rs start-python pid_control_node run pose waypoints &
+./dora-rs start-python control_node run control vehicle_id &
+# ./dora-rs start-python summary_node run control_status pose obstacles &
 # ./dora-rs start-python sink_eval_plot plot image waypoints obstacles pose &
+# for i in 1 2 3 4 5; do ./dora-rs start-python carla_source_node send && break || sleep 10; done
--- nodes/control_node.py
+++ nodes/control_node.py
@@ -12,6 +12,7 @@ client = Client(CARLA_SIMULATOR_HOST, int(CARLA_SIMULATOR_PORT))
 client.set_timeout(30.0)
 vehicle_id = None
 
+mutex = threading.Lock()
 
 def run(inputs):
     global vehicle_id
@@ -39,7 +40,8 @@ def run(inputs):
     )
 
     context = extract_context(inputs)
-    with tracer.start_span(f"within-python-{__name__}", context=context):
+
+    with tracer.start_span(f"within-python-{__name__}", context=context) as span:
         client.apply_batch_sync(
             [command.ApplyVehicleControl(vehicle_id, vec_control)]
         )
