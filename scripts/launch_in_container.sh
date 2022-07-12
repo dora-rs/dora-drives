689bd092d166ce70c1b18f0fc5f4318c127e8253
haixuanTao <tao.xavier@outlook.com>
2022-07-12 10:09:44 +0200

    restoring node based dataflow

--- scripts/launch_in_container.sh
+++ scripts/launch_in_container.sh
@@ -8,5 +8,5 @@ export PYTHONPATH=$PYTHONPATH:$(pwd)/nodes
 export DORA_RUNTIME_PATH=/home/dora/workspace/dora-rs/bin/dora-runtime
 export PATH=$PATH:/home/dora/workspace/dora-rs/bin/
 export DORA_PATH=/home/dora/workspace/dora-rs
-dora-coordinator run $DORA_PATH/graphs/drives_op_graph.yaml $DORA_RUNTIME_PATH
+dora-coordinator run $DORA_PATH/graphs/drives_node_graph.yaml $DORA_RUNTIME_PATH
 # ./dora-rs start-python sink_eval_plot plot image waypoints obstacles pose &
