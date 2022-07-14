3a8da608fc07a31409580c304ff79c3a9441d2aa
haixuanTao <tao.xavier@outlook.com>
2022-07-14 15:21:25 +0200

    removing `pylot.utils` from `perfect_detection_node`

---
+++ nodes/dora_utils.py
@@ -0,0 +1,107 @@
+import numpy as np
+
+
+def get_projection_matrix(position: np.array):
+    """Creates a transformation matrix to convert points in the 3D world
+    coordinate space with respect to the object.
+    Use the transform_points function to transpose a given set of points
+    with respect to the object.
+    Args:
+        position (np.array): [x, y, z, stir, yaw, roll]
+    Returns:
+        A 4x4 numpy matrix which represents the transformation matrix.
+    """
+    matrix = np.identity(4)
+    cy = np.cos(np.radians(position[4]))
+    sy = np.sin(np.radians(position[4]))
+    cr = np.cos(np.radians(position[5]))
+    sr = np.sin(np.radians(position[5]))
+    cp = np.cos(np.radians(position[3]))
+    sp = np.sin(np.radians(position[3]))
+    matrix[:3, 3] = position[:3]
+    matrix[0, 0] = cp * cy
+    matrix[0, 1] = cy * sp * sr - sy * cr
+    matrix[0, 2] = -1 * (cy * sp * cr + sy * sr)
+    matrix[1, 0] = sy * cp
+    matrix[1, 1] = sy * sp * sr + cy * cr
+    matrix[1, 2] = cy * sr - sy * sp * cr
+    matrix[2, 0] = sp
+    matrix[2, 1] = -1 * (cp * sr)
+    matrix[2, 2] = cp * cr
+    return matrix
+
+
+def to_world_coordinate(points, matrix):
+    """Internal function to transform the points according to the
+    given matrix. This function either converts the points from
+    coordinate space relative to the transform to the world coordinate
+    space (using self.matrix), or from world coordinate space to the
+    space relative to the transform (using inv(self.matrix))
+
+    Args:
+        points: An n by 3 numpy array, where each row is the
+            (x, y, z) coordinates of a point.
+        matrix: The matrix of the transformation to apply.
+
+    Returns:
+        An n by 3 numpy array of transformed points.
+    """
+    # Needed format: [[X0,..Xn],[Y0,..Yn],[Z0,..Zn]].
+    # So let's transpose the point matrix.
+    points = points.transpose()
+
+    # Add 1s row: [[X0..,Xn],[Y0..,Yn],[Z0..,Zn],[1,..1]]
+    points = np.append(points, np.ones((1, points.shape[1])), axis=0)
+
+    # Point transformation (depends on the given matrix)
+    points = np.dot(matrix, points)
+
+    # Get all but the last row in array form.
+    points = np.asarray(points[0:3].transpose()).astype(np.float16)
+
+    return points
+
+
+def get_extrinsic_matrix(transform):
+    """Converts a Transform from the camera coordinate space to the
+    Unreal coordinate space.
+    The camera space is defined as:
+        +x to right, +y to down, +z into the screen.
+    The unreal coordinate space is defined as:
+        +x into the screen, +y to right, +z to up.
+    Args:
+        transform (:py:class:`~pylot.utils.Transform`): The transform to
+            convert to Unreal coordinate space.
+    Returns:
+        :py:class:`~pylot.utils.Transform`: The given transform after
+            transforming to the Unreal coordinate space.
+    """
+
+    to_unreal_transform = np.array(
+        [[0, 0, 1, 0], [1, 0, 0, 0], [0, -1, 0, 0], [0, 0, 0, 1]]
+    )
+    return transform * to_unreal_transform
+
+
+def get_intrinsic_matrix(width: int, height: int, fov: float):
+    """Creates the intrinsic matrix for a camera with the given
+    parameters.
+    Args:
+        width (int): The width of the image returned by the camera.
+        height (int): The height of the image returned by the camera.
+        fov (float): The field-of-view of the camera.
+    Returns:
+        :py:class:`numpy.ndarray`: A 3x3 intrinsic matrix of the camera.
+    """
+
+    k = np.identity(3)
+    # We use width - 1 and height - 1 to find the center column and row
+    # of the image, because the images are indexed from 0.
+
+    # Center column of the image.
+    k[0, 2] = (width - 1) / 2.0
+    # Center row of the image.
+    k[1, 2] = (height - 1) / 2.0
+    # Focal length.
+    k[0, 0] = k[1, 1] = (width - 1) / (2.0 * np.tan(fov * np.pi / 360.0))
+    return k
--- nodes/perfect_detection_node.py
+++ nodes/perfect_detection_node.py
@@ -1,8 +1,14 @@
 import numpy as np
-from carla import Client
-from pylot.simulation.utils import extract_data_in_pylot_format
+from carla import Client, Vehicle, Walker
+from shapely.geometry import LineString
 
-from dora_watermark import dump, load
+from dora_utils import (
+    get_extrinsic_matrix,
+    get_intrinsic_matrix,
+    get_projection_matrix,
+    to_world_coordinate,
+)
+from dora_watermark import dump
 
 CARLA_SIMULATOR_HOST = "localhost"
 CARLA_SIMULATOR_PORT = "2000"
@@ -10,9 +16,333 @@ CARLA_SIMULATOR_PORT = "2000"
 client = Client(CARLA_SIMULATOR_HOST, int(CARLA_SIMULATOR_PORT))
 client.set_timeout(30.0)  # seconds
 world = client.get_world()
-
+SEGMENTATION_THRESHOLD = 0.20
+DEPTH_THRESHOLD = 5
 town_name = world.get_map().name
 DYNAMIC_OBSTACLE_DISTANCE_THRESHOLD = 500
+IMAGE_WIDTH = 800
+IMAGE_HEIGHT = 600
+DEPTH_IMAGE_WIDTH = 800
+DEPTH_IMAGE_HEIGHT = 600
+DEPTH_FOV = 90
+INTRINSIC_MATRIX = get_intrinsic_matrix(
+    DEPTH_IMAGE_WIDTH, DEPTH_IMAGE_HEIGHT, DEPTH_FOV
+)
+
+
+def threshold(p1, p2, camera_thresholds):
+    points = []
+    # If the points are themselves within the image, add them to the
+    # set of thresholded points.
+    if (
+        p1[0] >= 0
+        and p1[0] < IMAGE_WIDTH
+        and p1[1] >= 0
+        and p1[1] < IMAGE_HEIGHT
+    ):
+        points.append(p1)
+
+    if (
+        p2[0] >= 0
+        and p2[0] < IMAGE_WIDTH
+        and p2[1] >= 0
+        and p2[1] < IMAGE_HEIGHT
+    ):
+        points.append(p2)
+
+    # Compute the intersection of the line segment formed by p1 -- p2
+    # with all the thresholds of the camera image.
+    p12 = LineString((p1, p2))
+    for camera_threshold in camera_thresholds:
+        p = p12.intersection(camera_threshold)
+        if not p.is_empty:
+            if p.geom_type == "Point":
+                points.append((p.x, p.y))
+            elif p.geom_type == "LineString":
+                for coord in p.coords:
+                    points.append((coord[0], coord[1]))
+    return points
+
+
+def location_to_camera_view(location: np.array, extrinsic_matrix):
+    """Converts the given 3D vector to the view of the camera using
+    the extrinsic and the intrinsic matrix.
+    Args:
+        extrinsic_matrix: The extrinsic matrix of the camera.
+    Returns:
+        :py:class:`.Vector3D`: An instance with the coordinates converted
+        to the camera view.
+    """
+    position_vector = location.T
+    position_vector += [[1.0]]
+
+    # Transform the points to the camera in 3D.
+    transformed_3D_pos = np.dot(
+        np.linalg.inv(extrinsic_matrix), position_vector
+    )
+
+    # Transform the points to 2D.
+    position_2D = np.dot(INTRINSIC_MATRIX, transformed_3D_pos[:3])
+
+    # Normalize the 2D points.
+    location_2D = np.array(
+        [
+            float(position_2D[0] / position_2D[2]),
+            float(position_2D[1] / position_2D[2]),
+            float(position_2D[2]),
+        ]
+    )
+    return location_2D
+
+
+def to_camera_view(
+    bounding_box,
+    obstacle_projection: np.array,
+    extrinsic_matrix,
+):
+    """Converts the coordinates of the bounding box for the given obstacle
+    to the coordinates in the view of the camera.
+    This method retrieves the extent of the bounding box, transforms them
+    to coordinates relative to the bounding box origin, then converts those
+    to coordinates relative to the obstacle.
+    These coordinates are then considered to be in the world coordinate
+    system, which is mapped into the camera view. A negative z-value
+    signifies that the bounding box is behind the camera plane.
+    Note that this function does not cap the coordinates to be within the
+    size of the camera image.
+    Args:
+        obstacle_transform (:py:class:`~pylot.utils.Transform`): The
+            transform of the obstacle that the bounding box is associated
+            with.
+        extrinsic_matrix: The extrinsic matrix of the camera.
+    Returns:
+        A list of 8 Location instances specifying the 8 corners of the
+        bounding box.
+    """
+    # Retrieve the eight coordinates of the bounding box with respect to
+    # the origin of the bounding box.
+
+    extent = bounding_box.extent
+    location = bounding_box.location
+
+    # bounding box 8 coordinates relative to the obstacle
+    bbox = np.array(
+        [
+            [
+                location.x + extent.x,
+                location.y + extent.y,
+                location.z - extent.z,
+            ],
+            [
+                location.x - extent.x,
+                location.y + extent.y,
+                location.z - extent.z,
+            ],
+            [
+                location.x - extent.x,
+                location.y - extent.y,
+                location.z - extent.z,
+            ],
+            [
+                location.x + extent.x,
+                location.y - extent.y,
+                location.z - extent.z,
+            ],
+            [
+                location.x + extent.x,
+                location.y + extent.y,
+                location.z + extent.z,
+            ],
+            [
+                location.x - extent.x,
+                location.y + extent.y,
+                location.z + extent.z,
+            ],
+            [
+                location.x - extent.x,
+                location.y - extent.y,
+                location.z + extent.z,
+            ],
+            [
+                location.x + extent.x,
+                location.y - extent.y,
+                location.z + extent.z,
+            ],
+        ]
+    )
+
+    # Transform the vertices with respect to the bounding box transform.
+    bbox = to_world_coordinate(bbox, obstacle_projection)
+
+    # Obstacle's transform is relative to the world. Thus, the bbox
+    # contains the 3D bounding box vertices relative to the world.
+    camera_coordinates = []
+    for vertex in bbox:
+        location_2D = location_to_camera_view(vertex, extrinsic_matrix)
+
+        # Add the points to the image.
+        camera_coordinates.append(location_2D)
+
+    return camera_coordinates
+
+
+def get_bounding_box_in_camera_view(
+    bb_coordinates, image_width, image_height
+) -> np.array:
+    """Creates the bounding box in the view of the camera image using the
+    coordinates generated with respect to the camera transform.
+    Args:
+        bb_coordinates: 8 :py:class:`~pylot.utils.Location` coordinates of
+            the bounding box relative to the camera transform.
+        image_width (:obj:`int`): The width of the image being published by the
+            camera.
+        image_height (:obj:`int`): The height of the image being published by
+            the camera.
+    Returns:
+        np.array shape (1, 4): a bounding box, or None if the bounding box
+            does not fall into the view of the camera.
+    """
+    # Make sure that atleast 2 of the bounding box coordinates are in front.
+    z_vals = [loc.z for loc in bb_coordinates if loc.z >= 0]
+    if len(z_vals) < 2:
+        return None
+
+    # Create the thresholding line segments of the camera view.
+
+    left = LineString(((0, 0), (0, image_height)))
+    bottom = LineString(((0, image_height), (image_width, image_height)))
+    right = LineString(((image_width, image_height), (image_width, 0)))
+    top = LineString(((image_width, 0), (0, 0)))
+    camera_thresholds = [left, bottom, right, top]
+
+    # Go over each of the segments of the bounding box and threshold it to
+    # be inside the image.
+    thresholded_points = []
+    points = [(int(loc.x), int(loc.y)) for loc in bb_coordinates]
+    # Bottom plane thresholded.
+    thresholded_points.extend(
+        threshold(points[0], points[1], camera_thresholds)
+    )
+    thresholded_points.extend(
+        threshold(points[1], points[2], camera_thresholds)
+    )
+    thresholded_points.extend(
+        threshold(points[2], points[3], camera_thresholds)
+    )
+    thresholded_points.extend(
+        threshold(points[3], points[0], camera_thresholds)
+    )
+
+    # Top plane thresholded.
+    thresholded_points.extend(
+        threshold(points[4], points[5], camera_thresholds)
+    )
+    thresholded_points.extend(
+        threshold(points[5], points[6], camera_thresholds)
+    )
+    thresholded_points.extend(
+        threshold(points[6], points[7], camera_thresholds)
+    )
+    thresholded_points.extend(
+        threshold(points[7], points[4], camera_thresholds)
+    )
+
+    # Remaining segments thresholded.
+    thresholded_points.extend(
+        threshold(points[0], points[4], camera_thresholds)
+    )
+    thresholded_points.extend(
+        threshold(points[1], points[5], camera_thresholds)
+    )
+    thresholded_points.extend(
+        threshold(points[2], points[6], camera_thresholds)
+    )
+    thresholded_points.extend(
+        threshold(points[3], points[7], camera_thresholds)
+    )
+
+    if len(thresholded_points) == 0:
+        return None
+    else:
+        x = [int(x) for x, _ in thresholded_points]
+        y = [int(y) for _, y in thresholded_points]
+        if min(x) < max(x) and min(y) < max(y):
+            return np.array([min(x), max(x), min(y), max(y)])
+        else:
+            return None
+
+
+def populate_bounding_box_2D(
+    obstacle, depth_frame, segmented_frame, depth_frame_position
+) -> np.array:
+    """Populates the 2D bounding box for the obstacle.
+    Heuristically uses the depth frame and segmentation frame to figure out
+    if the obstacle is in view of the camera or not.
+    Args:
+        depth_frame (:py:class:`~pylot.perception.depth_frame.DepthFrame`):
+            Depth frame used to compare the depth to the distance of the
+            obstacle from the sensor.
+        segmented_frame (:py:class:`~pylot.perception.segmentation.segmented_frame.SegmentedFrame`):  # noqa: E501
+            Segmented frame used to refine the conversions.
+    Returns:
+       np.array: An instance representing a
+        rectangle over the obstacle if the obstacle is deemed to be
+        visible, None otherwise.
+    """
+
+    # Convert the bounding box of the obstacle to the camera coordinates.
+    bb_coordinates = to_camera_view(
+        obstacle.bounding_box,
+        get_projection_matrix(obstacle.transform),
+        get_extrinsic_matrix(get_projection_matrix(depth_frame_position)),
+    )
+
+    # Threshold the bounding box to be within the camera view.
+    bbox_2d = get_bounding_box_in_camera_view(
+        bb_coordinates,
+        DEPTH_IMAGE_WIDTH,
+        DEPTH_IMAGE_HEIGHT,
+    )
+
+    if not bbox_2d:
+        return None
+    # Crop the segmented and depth image to the given bounding box.
+    cropped_image = segmented_frame[
+        bbox_2d[2] : bbox_2d[3], bbox_2d[0] : bbox_2d[1]
+    ]
+    cropped_depth = depth_frame[
+        bbox_2d[2] : bbox_2d[3], bbox_2d[0] : bbox_2d[1]
+    ]
+
+    # Get the instance of the Obstacle
+    if isinstance(obstacle, Vehicle):
+        segmentation_class = 10
+    else:
+        segmentation_class = 4
+
+    # If the size of the bounding box is greater than 0, ensure that the
+    # bounding box contains more than a threshold of pixels corresponding
+    # to the required segmentation class.
+    if cropped_image.size > 0:
+        masked_image = np.zeros_like(cropped_image)
+        masked_image[np.where(cropped_image == segmentation_class)] = 1
+        seg_threshold = SEGMENTATION_THRESHOLD * masked_image.size
+        if np.sum(masked_image) >= seg_threshold:
+            # The bounding box contains the required number of pixels that
+            # belong to the required class. Ensure that the depth of the
+            # obstacle is the depth in the image.
+            masked_depth = cropped_depth[np.where(masked_image == 1)]
+            mean_depth = np.mean(masked_depth) * 1000
+            transform = obstacle.get_transform()
+            obs_x = transform.location.x
+            obs_y = transform.location.y
+            obs_z = transform.location.z
+            depth = np.linalg.norm(
+                [obs_x, obs_y, obs_z] - depth_frame_position[:3]
+            )
+            if abs(depth - mean_depth) <= DEPTH_THRESHOLD:
+                return bbox_2d
+    return None
 
 
 def dora_run(inputs):
@@ -27,28 +357,38 @@ def dora_run(inputs):
 
     position = np.frombuffer(inputs["position"])
 
-    depth_frame = load(inputs, "depth_frame")
-    segmented_frame = load(inputs, "segmented_frame")
+    buffer_depth_frame = inputs["depth_frame"]
+    buffer_segmented_frame = inputs["segmented_frame"]
+    depth_frame = np.frombuffer(
+        buffer_depth_frame[: 800 * 600 * 4], dtype="uint8"
+    )
+    segmented_frame = np.frombuffer(
+        buffer_segmented_frame[: 800 * 600 * 4], dtype="uint8"
+    )
+    depth_frame_position = np.frombuffer(
+        buffer_depth_frame[800 * 600 * 4 :], dtype="float32"
+    )
 
     actor_list = world.get_actors()
-    (vehicles, people, _traffic_lights, _, _) = extract_data_in_pylot_format(
-        actor_list
-    )
+    obstacles = actor_list.filter("walker.pedestrian.*|vehicle.*")
+
     det_obstacles = []
-    for obstacle in vehicles + people:
+    for obstacle in obstacles:
         # Calculate the distance of the obstacle from the vehicle, and
         # convert to camera view if it is less than
         # dynamic_obstacle_distance_threshold metres away.
-        obs_x = obstacle.transform.location.x
-        obs_y = obstacle.transform.location.y
-        obs_z = obstacle.transform.location.z
+        transform = obstacle.get_transform()
+        obs_x = transform.location.x
+        obs_y = transform.location.y
+        obs_z = transform.location.z
 
         if (
             np.linalg.norm([obs_x, obs_y, obs_z] - position[:3])
             <= DYNAMIC_OBSTACLE_DISTANCE_THRESHOLD
         ):
-            bbox = obstacle.populate_bounding_box_2D(
-                depth_frame, segmented_frame
+
+            bbox = populate_bounding_box_2D(
+                obstacle, depth_frame, segmented_frame, depth_frame_position
             )
             if bbox:
                 det_obstacles.append(obstacle)
