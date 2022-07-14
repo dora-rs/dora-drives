fa17a4fabf65843e6c26ddefa550e883afd5f683
haixuanTao <tao.xavier@outlook.com>
2022-07-14 09:50:07 +0200

    change frame into numpy `bytes`

--- nodes/carla_source_node.py
+++ nodes/carla_source_node.py
@@ -3,20 +3,9 @@ import random
 import time
 
 import numpy as np
-import pylot.simulation.utils
-import pylot.utils
 from carla import Client, Location, Rotation, Transform, command
-from pylot.drivers.sensor_setup import (
-    CameraSetup,
-    LidarSetup,
-    SegmentedCameraSetup,
-)
-from pylot.perception.camera_frame import CameraFrame
-from pylot.perception.depth_frame import DepthFrame
-from pylot.perception.point_cloud import PointCloud
-from pylot.perception.segmentation.segmented_frame import SegmentedFrame
 
-from dora_watermark import dump, load
+from dora_watermark import dump
 
 logger = logging.Logger("")
 
@@ -29,7 +18,7 @@ IMAGE_HEIGHT = 600
 
 lidar_pc = None
 depth_frame = None
-last_frame = None
+camera_frame = None
 segmented_frame = None
 vehicle_id = None
 
@@ -37,70 +26,80 @@ sensor_transform = Transform(
     Location(3, 0, 1), Rotation(pitch=0, yaw=0, roll=0)
 )
 
-goal_location = pylot.utils.Location(234, 59, 39)
 
+def on_segmented_msg(frame):
+    location = frame.transform.location
+    rotation = frame.transform.rotation
+    x = location.x
+    y = location.y
+    z = location.z
+    pitch = rotation.pitch
+    yaw = rotation.yaw
+    roll = rotation.roll
 
-def on_segmented_msg(simulator_image):
-    transform = pylot.utils.Transform.from_simulator_transform(
-        simulator_image.transform
-    )
-    camera_setup = SegmentedCameraSetup(
-        "segmented_camera",
-        800,
-        600,
-        transform,
-        90.0,
-    )
+    camera_data = np.array(
+        [x, y, z, pitch, yaw, roll], dtype="float32"
+    ).tobytes()
 
     global segmented_frame
-    segmented_frame = SegmentedFrame.from_simulator_image(
-        simulator_image, camera_setup
-    )
+    segmented_frame = frame.raw_data.tobytes() + camera_data
 
 
-def on_lidar_msg(simulator_pc):
-    lidar_transform = pylot.utils.Transform.from_simulator_transform(
-        simulator_pc.transform
-    )
-    lidar_setup = LidarSetup(
-        "lidar", lidar_type="sensor.lidar.ray_cast", transform=lidar_transform
-    )
+def on_lidar_msg(frame):
+    location = frame.transform.location
+    rotation = frame.transform.rotation
+    x = location.x
+    y = location.y
+    z = location.z
+    pitch = rotation.pitch
+    yaw = rotation.yaw
+    roll = rotation.roll
+
+    camera_data = np.array(
+        [x, y, z, pitch, yaw, roll], dtype="float32"
+    ).tobytes()
 
     global lidar_pc
-    lidar_pc = PointCloud.from_simulator_point_cloud(simulator_pc, lidar_setup)
-    # pptk.viewer(point_cloud.points)
+    lidar_pc = frame.raw_data.tobytes() + camera_data
 
 
-def on_camera_msg(simulator_image):
+def on_camera_msg(frame):
+    location = frame.transform.location
+    rotation = frame.transform.rotation
+    x = location.x
+    y = location.y
+    z = location.z
+    pitch = rotation.pitch
+    yaw = rotation.yaw
+    roll = rotation.roll
 
-    camera_transform = pylot.utils.Transform.from_simulator_transform(
-        simulator_image.transform
-    )
+    camera_data = np.array(
+        [x, y, z, pitch, yaw, roll], dtype="float32"
+    ).tobytes()
 
-    camera_setup = CameraSetup(
-        "rgb_camera", "sensor.camera.rgb", 800, 600, camera_transform, fov=90.0
-    )
-
-    global last_frame
-    last_frame = CameraFrame.from_simulator_frame(simulator_image, camera_setup)
+    global camera_frame
+    camera_frame = frame.raw_data.tobytes() + camera_data
 
 
-def on_depth_msg(simulator_image):
+def on_depth_msg(frame):
+    # _frame = np.frombuffer(frame.raw_data, dtype=np.dtype("uint8"))
+    # _frame = np.reshape(_frame, (frame.height, frame.width, 4))
 
-    depth_camera_transform = pylot.utils.Transform.from_simulator_transform(
-        simulator_image.transform
-    )
+    location = frame.transform.location
+    rotation = frame.transform.rotation
+    x = location.x
+    y = location.y
+    z = location.z
+    pitch = rotation.pitch
+    yaw = rotation.yaw
+    roll = rotation.roll
 
-    camera_setup = CameraSetup(
-        "depth_camera",
-        "sensor.camera.depth",
-        800,
-        600,
-        depth_camera_transform,
-        fov=90.0,
-    )
+    camera_data = np.array(
+        [x, y, z, pitch, yaw, roll], dtype="float32"
+    ).tobytes()
+
     global depth_frame
-    depth_frame = DepthFrame.from_simulator_frame(simulator_image, camera_setup)
+    depth_frame = frame.raw_data.tobytes() + camera_data
 
     # pptk.viewer(depth_pc)
 
@@ -209,9 +208,231 @@ world = client.get_world()
 # settings.synchronous_mode = True
 # settings.fixed_delta_seconds = 1.0 / 10
 # world.apply_settings(settings)
+def check_simulator_version(
+    simulator_version: str,
+    required_major: int = 0,
+    required_minor: int = 9,
+    required_patch: int = 1,
+):
+    """Checks if the simulator meets the minimum version requirements."""
+    ver_strs = simulator_version.split(".")
+    if len(ver_strs) < 2 or len(ver_strs) > 3:
+        print(
+            "WARNING: Assuming that installed CARLA {} API is compatible "
+            "with CARLA 0.9.10 API".format(simulator_version)
+        )
+        ver_strs = "0.9.10".split(".")
+    major = int(ver_strs[0])
+    minor = int(ver_strs[1])
+    if major != required_major:
+        return major > required_major
+    else:
+        if minor != required_minor:
+            return minor > required_minor
+        else:
+            if len(ver_strs) == 3:
+                patch = int(ver_strs[2])
+                return patch >= required_patch
+            else:
+                return True
+
+
+def spawn_actors(
+    client,
+    world,
+    traffic_manager_port: int,
+    simulator_version: str,
+    ego_spawn_point_index: int,
+    auto_pilot: bool,
+    num_people: int,
+    num_vehicles: int,
+    logger,
+):
+    vehicle_ids = spawn_vehicles(
+        client, world, traffic_manager_port, num_vehicles, logger
+    )
+    ego_vehicle = spawn_ego_vehicle(
+        world, traffic_manager_port, ego_spawn_point_index, auto_pilot
+    )
+    people = []
+
+    if check_simulator_version(
+        simulator_version, required_minor=9, required_patch=6
+    ):
+        # People do not move in versions older than 0.9.6.
+        (people, people_control_ids) = spawn_people(
+            client, world, num_people, logger
+        )
+        people_actors = world.get_actors(people_control_ids)
+        for i, ped_control_id in enumerate(people_control_ids):
+            # Start person.
+            people_actors[i].start()
+            people_actors[i].go_to_location(
+                world.get_random_location_from_navigation()
+            )
+    return ego_vehicle, vehicle_ids, people
+
+
+def spawn_ego_vehicle(
+    world,
+    traffic_manager_port: int,
+    spawn_point_index: int,
+    auto_pilot: bool,
+    blueprint: str = "vehicle.lincoln.mkz2017",
+):
+    v_blueprint = world.get_blueprint_library().filter(blueprint)[0]
+    ego_vehicle = None
+    while not ego_vehicle:
+        if spawn_point_index == -1:
+            # Pick a random spawn point.
+            start_pose = random.choice(world.get_map().get_spawn_points())
+        else:
+            spawn_points = world.get_map().get_spawn_points()
+            assert spawn_point_index < len(spawn_points), (
+                "Spawn point index is too big. "
+                "Town does not have sufficient spawn points."
+            )
+            start_pose = spawn_points[spawn_point_index]
+
+        ego_vehicle = world.try_spawn_actor(v_blueprint, start_pose)
+    if auto_pilot:
+        ego_vehicle.set_autopilot(True, traffic_manager_port)
+    return ego_vehicle
+
+
+def spawn_people(client, world, num_people: int, logger):
+    """Spawns people at random locations inside the world.
+
+    Args:
+        num_people: The number of people to spawn.
+    """
+    from carla import Transform, command
+
+    p_blueprints = world.get_blueprint_library().filter("walker.pedestrian.*")
+    unique_locs = set([])
+    spawn_points = []
+    # Get unique spawn points.
+    for i in range(num_people):
+        attempt = 0
+        while attempt < 10:
+            spawn_point = Transform()
+            loc = world.get_random_location_from_navigation()
+            if loc is not None:
+                # Transform to tuple so that location is comparable.
+                p_loc = (loc.x, loc.y, loc.z)
+                if p_loc not in unique_locs:
+                    spawn_point.location = loc
+                    spawn_points.append(spawn_point)
+                    unique_locs.add(p_loc)
+                    break
+            attempt += 1
+        if attempt == 10:
+            logger.error("Could not find unique person spawn point")
+    # Spawn the people.
+    batch = []
+    for spawn_point in spawn_points:
+        p_blueprint = random.choice(p_blueprints)
+        if p_blueprint.has_attribute("is_invincible"):
+            p_blueprint.set_attribute("is_invincible", "false")
+        batch.append(command.SpawnActor(p_blueprint, spawn_point))
+    # Apply the batch and retrieve the identifiers.
+    ped_ids = []
+    for response in client.apply_batch_sync(batch, True):
+        if response.error:
+            logger.info(
+                "Received an error while spawning a person: {}".format(
+                    response.error
+                )
+            )
+        else:
+            ped_ids.append(response.actor_id)
+    # Spawn the person controllers
+    ped_controller_bp = world.get_blueprint_library().find(
+        "controller.ai.walker"
+    )
+    batch = []
+    for ped_id in ped_ids:
+        batch.append(command.SpawnActor(ped_controller_bp, Transform(), ped_id))
+    ped_control_ids = []
+    for response in client.apply_batch_sync(batch, True):
+        if response.error:
+            logger.info(
+                "Error while spawning a person controller: {}".format(
+                    response.error
+                )
+            )
+        else:
+            ped_control_ids.append(response.actor_id)
+
+    return (ped_ids, ped_control_ids)
+
+
+def spawn_vehicles(
+    client, world, traffic_manager_port: int, num_vehicles: int, logger
+):
+    """Spawns vehicles at random locations inside the world.
+
+    Args:
+        num_vehicles: The number of vehicles to spawn.
+    """
+    from carla import command
+
+    logger.debug("Trying to spawn {} vehicles.".format(num_vehicles))
+    # Get the spawn points and ensure that the number of vehicles
+    # requested are less than the number of spawn points.
+    spawn_points = world.get_map().get_spawn_points()
+    if num_vehicles >= len(spawn_points):
+        logger.warning(
+            "Requested {} vehicles but only found {} spawn points".format(
+                num_vehicles, len(spawn_points)
+            )
+        )
+        num_vehicles = len(spawn_points)
+    else:
+        random.shuffle(spawn_points)
+
+    # Get all the possible vehicle blueprints inside the world.
+    v_blueprints = world.get_blueprint_library().filter("vehicle.*")
+
+    # Construct a batch message that spawns the vehicles.
+    batch = []
+    for transform in spawn_points[:num_vehicles]:
+        blueprint = random.choice(v_blueprints)
+
+        # Change the color of the vehicle.
+        if blueprint.has_attribute("color"):
+            color = random.choice(
+                blueprint.get_attribute("color").recommended_values
+            )
+            blueprint.set_attribute("color", color)
+
+        # Let the vehicle drive itself.
+        blueprint.set_attribute("role_name", "autopilot")
+
+        batch.append(
+            command.SpawnActor(blueprint, transform).then(
+                command.SetAutopilot(
+                    command.FutureActor, True, traffic_manager_port
+                )
+            )
+        )
+
+    # Apply the batch and retrieve the identifiers.
+    vehicle_ids = []
+    for response in client.apply_batch_sync(batch, True):
+        if response.error:
+            logger.info(
+                "Received an error while spawning a vehicle: {}".format(
+                    response.error
+                )
+            )
+        else:
+            vehicle_ids.append(response.actor_id)
+    return vehicle_ids
+
 
 # Spawn the vehicle.
-(_, vehicle_ids, people) = pylot.simulation.utils.spawn_actors(
+(_, vehicle_ids, people) = spawn_actors(
     client,
     world,
     8000,
@@ -236,11 +457,11 @@ segmented_camera = add_segmented_camera(
 
 def dora_run(_):
 
-    global last_frame
+    global camera_frame
     global segmented_frame
     global depth_frame
 
-    if last_frame is None or segmented_frame is None or depth_frame is None:
+    if camera_frame is None or segmented_frame is None or depth_frame is None:
         return {}
 
     vec_transform = ego_vehicle.get_transform()
@@ -262,9 +483,9 @@ def dora_run(_):
     position = np.array([x, y, z, pitch, yaw, roll, forward_speed])
 
     return {
-        "image": dump(last_frame),
-        "depth_frame": dump(depth_frame),
-        "segmented_frame": dump(segmented_frame),
+        "image": camera_frame,
+        "depth_frame": depth_frame,
+        "segmented_frame": segmented_frame,
         "position": position.tobytes(),
         "vehicle_id": vehicle_id.to_bytes(2, "big")
         #  "open_drive": world.get_map().to_opendrive().encode("utf-8"),
