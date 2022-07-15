7a4debc46d02ba80c1168e47e2e9c5d7454bd6a6
haixuanTao <tao.xavier@outlook.com>
2022-07-15 17:24:15 +0200

    Removing `pylot.utils` from `obstacle _location`

--- nodes/obstacle_location_node.py
+++ nodes/obstacle_location_node.py
@@ -1,17 +1,62 @@
 import logging
 
 import numpy as np
-import pylot.utils
-from pylot.perception.tracking.obstacle_trajectory import ObstacleTrajectory
-from pylot.prediction.obstacle_prediction import ObstaclePrediction
+from sklearn.metrics import pairwise_distances_argmin
 
-from dora_utils import get_projection_matrix
-from dora_watermark import dump, load
+from dora_utils import (
+    get_extrinsic_matrix,
+    get_intrinsic_matrix,
+    get_projection_matrix,
+    to_world_coordinate,
+)
 
 logger = logging.Logger("Obstacle Location")
+DEPTH_CAMERA_MAX_DEPTH = 1000
+DEPTH_IMAGE_WIDTH = 800
+DEPTH_IMAGE_HEIGHT = 600
+PIXEL_LENGTH = DEPTH_IMAGE_WIDTH * DEPTH_IMAGE_HEIGHT
+DEPTH_FOV = 90
+INTRINSIC_MATRIX = get_intrinsic_matrix(
+    DEPTH_IMAGE_WIDTH, DEPTH_IMAGE_HEIGHT, DEPTH_FOV
+)
 
 
-def get_predictions(obstacles, position_matrix):
+def get_point_cloud(depth_frame, depth_frame_matrix):
+    """Converts the depth frame to a 1D array containing the 3D
+    position of each pixel in world coordinates.
+    See :py:class:`~pylot.drivers.sensor_setup.CameraSetup` for
+    coordinate axis orientations.
+    """
+    frame = depth_frame.astype(np.float32)
+    # Apply (R + G * 256 + B * 256 * 256) / (256 * 256 * 256 - 1).
+    frame = np.dot(frame[:, :, :3], [65536.0, 256.0, 1.0])
+    frame /= 16777215.0  # (256.0 * 256.0 * 256.0 - 1.0)
+
+    # 2d pixel coordinates
+    u_coord = np.libmat.repmat(
+        np.r_[0:DEPTH_IMAGE_WIDTH:1], DEPTH_IMAGE_HEIGHT, 1
+    ).reshape(PIXEL_LENGTH)
+    v_coord = np.libmat.repmat(
+        np.c_[0:DEPTH_IMAGE_HEIGHT:1], 1, DEPTH_IMAGE_WIDTH
+    ).reshape(PIXEL_LENGTH)
+    normalized_depth = np.reshape(frame, PIXEL_LENGTH)
+
+    # p2d = [u,v,1]
+    p2d = np.array([u_coord, v_coord, np.ones_like(u_coord)])
+
+    # P = [X,Y,Z]
+    p3d = np.dot(np.linalg.inv(INTRINSIC_MATRIX), p2d)
+    p3d *= normalized_depth * DEPTH_CAMERA_MAX_DEPTH
+
+    # [[X1,Y1,Z1],[X2,Y2,Z2], ... [Xn,Yn,Zn]]
+    locations = np.asarray(np.transpose(p3d))
+    # Transform the points in 3D world coordinates.
+    extrinsic_matrix = get_extrinsic_matrix(depth_frame_matrix)
+    point_cloud = to_world_coordinate(locations, extrinsic_matrix)
+    return point_cloud
+
+
+def get_predictions(obstacles, obstacle_with_locations, position_matrix):
     """Extracts obstacle predictions out of the message.
     This method is useful to build obstacle predictions when
     the operator directly receives detections instead of predictions.
@@ -19,22 +64,12 @@ def get_predictions(obstacles, position_matrix):
     """
     predictions = []
     # Transform the obstacle into a prediction.
-    for obstacle in obstacles:
-        obstacle_trajectory = ObstacleTrajectory(obstacle, [])
-        inverse_matrix = np.linalg.inv(position_matrix)
-        prediction = ObstaclePrediction(
-            obstacle_trajectory,
-            obstacle.transform,
-            1.0,
-            [
-                pylot.utils.Transform(
-                    matrix=np.dot(inverse_matrix, obstacle.transform.matrix)
-                )
-            ],
-        )
-        predictions.append(prediction)
+    for obstacle, location in zip(obstacles, obstacle_with_locations):
+        obstacle_bytes = np.array([location]).tobytes()
+        obstacle_bytes += obstacle[-2:].tobytes()
 
-    return predictions
+    predictions_bytes = b"\n".join(predictions)
+    return predictions_bytes
 
 
 def get_obstacle_locations(
@@ -49,34 +84,37 @@ def get_obstacle_locations(
         get_projection_matrix(depth_frame_position),
     )
 
+    point_cloud = get_point_cloud(depth_frame, depth_frame_matrix)
+
+    obstacle_with_locations = []
     for obstacle in obstacles:
-        center_point = obstacle.bounding_box_2D.get_center_point()
         # Sample several points around the center of the bounding box
         # in case the bounding box is not well centered on the obstacle.
         # In such situations the center point might be in between legs,
         # and thus we might overestimate the distance.
         sample_points = []
-        for delta_x in range(-30, 30, 5):
-            for delta_y in range(-30, 30, 5):
-                sample_point = center_point + pylot.utils.Vector2D(
-                    delta_x, delta_y
-                )
-                if obstacle.bounding_box_2D.is_within(sample_point):
-                    sample_points.append(sample_point)
-        locations = depth_frame.get_pixel_locations(sample_points)
-        # Choose the closest from the locations of the sampled points.
-        min_distance = np.infty
-        closest_location = None
-        for location in locations:
-
-            dist = np.linalg.norm(location - ego_position[:3])
-            if dist < min_distance:
-                min_distance = dist
-                closest_location = location
-        obstacle.transform = pylot.utils.Transform(
-            closest_location, pylot.utils.Rotation()
+        coefficient = (
+            (obstacle[1] - obstacle[0]) / 2,
+            (obstacle[3] - obstacle[2]) / 2,
         )
-    return obstacles
+        left_corner = (
+            (obstacle[1] + obstacle[0]) / 2,
+            (obstacle[3] + obstacle[2]) / 2,
+        )
+        sample_points = (
+            np.random.uniform(size=(32, 2)) * coefficient + left_corner
+        )
+        sample_points = sample_points.astype(int)
+
+        locations = point_cloud[sample_points]
+
+        # Choose the closest from the locations of the sampled points.
+
+        min_location = pairwise_distances_argmin(locations, [ego_position[:3]])
+
+        obstacle_with_locations.append(locations[min_location])
+
+    return obstacle_with_locations
 
 
 def dora_run(inputs):
@@ -89,7 +127,11 @@ def dora_run(inputs):
     ):
         return {}
 
-    obstacles = load(inputs, "obstacles_without_location")
+    obstacles = np.frombuffer(
+        inputs["obstacles_without_location"], dtype="float32"
+    )
+    obstacles = obstacles.reshape((-1, 12))
+
     buffer_depth_frame = inputs["depth_frame"]
     depth_frame = np.frombuffer(
         buffer_depth_frame[: 800 * 600 * 4], dtype="uint8"
@@ -106,10 +148,8 @@ def dora_run(inputs):
         obstacles, depth_frame, position, depth_frame_position
     )
 
-    obstacles_with_prediction = get_predictions(
-        obstacles_with_location, position_matrix
+    predictions_bytes = get_predictions(
+        obstacles, obstacles_with_location, position_matrix
     )
 
-    obstacles = dump(obstacles_with_prediction)
-
-    return {"obstacles": obstacles}
+    return {"obstacles": predictions_bytes}
