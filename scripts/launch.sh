1db81b19a7ac5c6fa7b361bdf3546f62d505a8fd
haixuanTao <tao.xavier@outlook.com>
2022-08-03 11:06:09 +0200

    fix script

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,19 +1,19 @@
 cd ../dora
 export RUSTFLAGS="--cfg tokio_unstable"
-cargo build  --manifest-path binaries/coordinator/Cargo.toml --release # --features metrics # opentelemetry_jaeger 
-cargo build  --manifest-path binaries/runtime/Cargo.toml --release # --features metrics # opentelemetry_jaeger 
+# cargo build  --manifest-path binaries/coordinator/Cargo.toml --release # --features metrics # opentelemetry_jaeger 
+# cargo build  --manifest-path binaries/runtime/Cargo.toml --release # --features metrics # opentelemetry_jaeger 
 cd ../dora-drives
-cargo build --release
 cp ../dora/target/release/dora-coordinator bin/dora-coordinator
 cp ../dora/target/release/dora-runtime bin/dora-runtime
+cp -r ../dora/target/wheels .
 cp target/release/dora-drives-node bin/dora-drives-node
 
 # Carla start
 nvidia-docker build --tag dora .
 nvidia-docker run -itd --name dora -p 20022:22  dora /bin/bash
-nvidia-docker exec -itd dora /home/dora/workspace/dora-rs/scripts/run_simulator.sh
+nvidia-docker exec -itd dora /home/dora/workspace/dora-drives/scripts/run_simulator.sh
 nvidia-docker cp ~/.ssh/id_rsa.pub dora:/home/dora/.ssh/authorized_keys
 nvidia-docker exec -i -t dora sudo chown dora /home/dora/.ssh/authorized_keys
 nvidia-docker exec -i -t dora sudo service ssh start
 ssh -p 20022 -X dora@localhost 
-# nvidia-docker exec -it dora /home/dora/workspace/dora-rs/launch_in_container.sh
+# nvidia-docker exec -it dora /home/dora/workspace/dora-drives/launch_in_container.sh
--- scripts/launch_in_container.sh
+++ scripts/launch_in_container.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # InfluxDB
 export INFLUX_TOKEN=RmC0MizRVFt04yBDXE12nqBIwUVvBEVb1qAYBbX3OFwZYSnpiGPPc03SUGfM4MSwprlknimeXOqWGb6CXWq8ZA==
-~/workspace/dora-rs/telegraf-1.22.4/usr/bin/telegraf --config https://eu-central-1-1.aws.cloud2.influxdata.com/api/v2/telegrafs/09671055edbf6000 &
+~/workspace/dora-drives/telegraf-1.22.4/usr/bin/telegraf --config https://eu-central-1-1.aws.cloud2.influxdata.com/api/v2/telegrafs/09671055edbf6000 &
 
-cd /home/dora/workspace/dora-rs
-export PYTHONPATH=$PYTHONPATH:$(pwd)/nodes
-export DORA_RUNTIME_PATH=/home/dora/workspace/dora-rs/bin/dora-runtime
-export PATH=$PATH:/home/dora/workspace/dora-rs/bin/
-export DORA_PATH=/home/dora/workspace/dora-rs
+cd /home/dora/workspace/dora-drives
+export PYTHONPATH=$PYTHONPATH:$(pwd)/nodes:$(pwd)/operators
+export DORA_RUNTIME_PATH=/home/dora/workspace/dora-drives/bin/dora-runtime
+export PATH=$PATH:/home/dora/workspace/dora-drives/bin/
+export DORA_PATH=/home/dora/workspace/dora-drives
 dora-coordinator run $DORA_PATH/graphs/drives_node_graph.yaml $DORA_RUNTIME_PATH
-# ./dora-rs start-python sink_eval_plot plot image waypoints obstacles pose &
+# ./dora-drives start-python sink_eval_plot plot image waypoints obstacles pose &
