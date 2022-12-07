Fix/euler to quaternion angle (#19)
* add ros node written for dora-rover

* Use precompiled dora v0.1.1

This commit replace from source compilation to precompiled version of dora-rs.

See: https://github.com/dora-rs/dora/releases/tag/v0.1.1

* Implement `dora-rover` logic

This commit introduce a lot of the logic required to run a real environment
rover.

It changes the basic localisation from euler angle to quaternion as it is
more robust.

It also transform the planner from `hybrid_astar` to `frenet`.

It add a bit of visualisation information concerning the position and control output.

It removes the precalculated speed to an estimated speed.

carla/_dora_utils.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 import numpy as np
 from sklearn.metrics import pairwise_distances
-
+from typing import Tuple
 
 def distance_vertex(left_vertix: np.array, right_vertix: np.array) -> np.array:
     return np.linalg.norm(left_vertix[:3] - right_vertix[:3])
@@ -13,7 +13,7 @@ def distance_points(left_point: np.array, right_point: np.array) -> np.array:
     return np.linalg.norm(left_point - right_point)
 
 
-def closest_vertex(vertices: np.array, point: np.array) -> (int, np.array):
+def closest_vertex(vertices: np.array, point: np.array) -> Tuple[int, np.array]:
     assert (
         vertices.shape[1] == point.shape[1]
     ), "vertice has more coordinate than point"

carla/carla_source_node.py
@@ -16,6 +16,7 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.trace.propagation.tracecontext import (
     TraceContextTextMapPropagator,
 )
+from scipy.spatial.transform import Rotation as R
 
 from _generate_world import (
     add_camera,
@@ -26,6 +27,22 @@ from _generate_world import (
     spawn_driving_vehicle,
 )
 from carla import Client, Location, Rotation, Transform
+
+
+def euler_to_quaternion(yaw, pitch, roll):
+    qx = np.sin(roll / 2) * np.cos(pitch / 2) * np.cos(yaw / 2) - np.cos(
+        roll / 2
+    ) * np.sin(pitch / 2) * np.sin(yaw / 2)
+    qy = np.cos(roll / 2) * np.sin(pitch / 2) * np.cos(yaw / 2) + np.sin(
+        roll / 2
+    ) * np.cos(pitch / 2) * np.sin(yaw / 2)
+    qz = np.cos(roll / 2) * np.cos(pitch / 2) * np.sin(yaw / 2) - np.sin(
+        roll / 2
+    ) * np.sin(pitch / 2) * np.cos(yaw / 2)
+    qw = np.cos(roll / 2) * np.cos(pitch / 2) * np.cos(yaw / 2) + np.sin(
+        roll / 2
+    ) * np.sin(pitch / 2) * np.sin(yaw / 2)
+    return [qx, qy, qz, qw]
 
 
 def serialize_context(context: dict) -> str:
@@ -82,7 +99,11 @@ def on_segmented_msg(frame):
 def on_lidar_msg(frame):
 
     global lidar_pc
-    lidar_pc = zlib.compress(frame.raw_data.tobytes())
+    frame = np.frombuffer(frame.raw_data, dtype=np.dtype("float32"))
+    point_cloud = np.reshape(frame, (-1, 4))
+    point_cloud = point_cloud[:, :3]
+
+    lidar_pc = point_cloud.tobytes()
 
 
 def on_camera_msg(frame):
@@ -143,8 +164,8 @@ node.send_output("vehicle_id", vehicle_id.to_bytes(2, "big"))
 def main():
 
     global camera_frame
-    global segmented_frame
-    global depth_frame
+    # global segmented_frame
+    # global depth_frame
     global lidar_pc
 
     if camera_frame is None or segmented_frame is None or depth_frame is None:
@@ -155,28 +176,25 @@ def main():
     x = vec_transform.location.x
     y = vec_transform.location.y
     z = vec_transform.location.z
-    pitch = vec_transform.rotation.pitch
     yaw = vec_transform.rotation.yaw
+    pitch = vec_transform.rotation.pitch
     roll = vec_transform.rotation.roll
 
-    vx = velocity_vector.x
-    vy = velocity_vector.y
-    vz = velocity_vector.z
+    [[qx, qy, qz, qw]] = R.from_euler(
+        "xyz", [[roll, pitch, yaw]], degrees=True
+    ).as_quat()
 
-    forward_speed = np.linalg.norm([vx, vy, vz])
-
-    position = np.array([x, y, z, pitch, yaw, roll, forward_speed])
-    with tracer.start_as_current_span("source") as _span:
-        output = {}
-        propagator.inject(output)
-        metadata = {"open_telemetry_context": serialize_context(output)}
-        node.send_output("position", position.tobytes(), metadata)
-        node.send_output("image", camera_frame, metadata)
-        node.send_output("depth_frame", depth_frame, metadata)
-        node.send_output("segmented_frame", segmented_frame, metadata)
-        node.send_output("lidar_pc", lidar_pc, metadata)
+    position = np.array([x, y, z, qx, qy, qz, qw], np.float32)
+    # with tracer.start_as_current_span("source") as _span:
+    output = {}
+    propagator.inject(output)
+    metadata = {"open_telemetry_context": serialize_context(output)}
+    node.send_output("position", position.tobytes(), metadata)
+    node.send_output("image", camera_frame, metadata)
+    # node.send_output("depth_frame", depth_frame, metadata)
+    # node.send_output("segmented_frame", segmented_frame, metadata)
+    node.send_output("lidar_pc", lidar_pc, metadata)
 
 
-for _ in range(1000):
-    time.sleep(0.3)
+for input_id, value, metadata in node:
     main()

carla/perfect_detection_op.py
@@ -329,7 +329,7 @@ class Operator:
             return DoraStatus.CONTINUE
 
         elif dora_input["id"] == "position":
-            self.position = np.frombuffer(dora_input["data"])[:6]
+            self.position = np.frombuffer(dora_input["data"], np.float32)
 
         if (
             len(self.position) == 0