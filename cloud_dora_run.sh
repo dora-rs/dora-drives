68472b4ac4a4882ba7f3c13cbe6a37f2b99b132f
haixuanTao <tao.xavier@outlook.com>
2023-04-11 11:27:49 +0800

    Remove `dora_jaeger` as default env variable

--- cloud_dora_run.sh
+++ cloud_dora_run.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 # export DESTINATION=`cat $DEST`
-export DORA_JAEGER_TRACING=172.17.0.1:6831
+# export DORA_JAEGER_TRACING=172.17.0.1:6831
 dora up
 dora start $YAML --attach --hot-reload
