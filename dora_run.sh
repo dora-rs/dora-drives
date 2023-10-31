6d3534a22943550bafc85529197d02f381ad1f25
haixuanTao <tao.xavier@outlook.com>
2023-10-31 16:36:53 +0100

    Use absolute path for upgrade dora script

--- dora_run.sh
+++ dora_run.sh
@@ -2,7 +2,7 @@
 export DESTINATION=`cat $DEST`
 # export DORA_JAEGER_TRACING=172.17.0.1:6831
 
-./upgrade_dora.sh
+/home/dora/workspace/simulate/team_code/dora-drives/upgrade_dora.sh
 
 dora up
 dora start $YAML --attach --hot-reload
